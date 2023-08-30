a4a274c56bc554b25ab4d58e4b592df25c25f6e2
Badri Venkata Durga Praveen <badrivdpraveen5@gmail.com>
2023-08-30 22:23:51 +0530

    second commit

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ def home():
 def about():
     return render_template('about.html')
 
-
+@app.route('/story')
+def story():
+    return render_template('story.html')
 
 if __name__ =='__main__':
     app.run( debug = True)
