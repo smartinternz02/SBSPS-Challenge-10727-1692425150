b5feb5ce3878d91aaa93aa58971ce16a89d431e4
Badri Venkata Durga Praveen <badrivdpraveen5@gmail.com>
2023-08-30 18:09:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request,session
+app = Flask(__name__)
+app.secret_key ='a'
+
+print("Flask running")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/analytics')
+def analytics():
+    return render_template('analytics.html')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+if __name__ =='__main__':
+    app.run( debug = True)
